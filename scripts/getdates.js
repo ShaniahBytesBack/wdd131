c4a70c958497ff629bac7a2acc7e325a9e9bd4f3
ShaniahBytesBack <nya24008@byui.edu>
2024-11-11 15:33:50 +0200

    additions to the site

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Insert current year into footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Insert last modified date into footer
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
